ea5724c7cedd64e9d76277815a529ce8adb1be0f
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2021-01-11 09:47:15 +0100

    fixed beug empty message

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { emojify } from 'react-emojione';
 
 function strToRGB(str) {
-  if (str === undefined) {
-    return null;
-  }
   let hash = 0;
   for (let i = 0; i < str.length; i += 1) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
--- src/containers/message_form.jsx
+++ src/containers/message_form.jsx
@@ -6,28 +6,49 @@ import { createMessage } from '../actions/index';
 class MessageForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = {
+      value: '',
+      errors: {}
+    };
   }
 
   componentDidMount() {
-    this.messageBox.focus();
+    this.messageBox.focus(); // permet de mettre le curseur dans l'input de form automatiquement grace a ref apres que le component soit monté.
   }
 
   handleChange = (event) => {
     this.setState({ value: event.target.value });
   }
 
+  handleValidation = () => {
+    let value = this.state.value;
+    let errors = {};
+    let formIsValid = true;
+
+    if (value === '') {
+      formIsValid = false;
+      errors["value"] = "Cannot be empty";
+    }
+
+    this.setState({errors: errors});
+    return formIsValid;
+  }
+
   handleSubmit = (event) => {
     event.preventDefault();
-    this.props.createMessage(this.props.selectedChannel, this.props.currentUser, this.state.value);
-    this.setState({ value: '' }); // Reset message input
+    if (this.handleValidation()) {
+      this.props.createMessage(this.props.selectedChannel, this.props.currentUser, this.state.value);
+      this.setState({ value: '' }); // Reset message input
+    } else {
+      alert("Message cannot be empty.");
+    }
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit} className="channel-editor">
         <input
-          ref={(input) => { this.messageBox = input; }}
+          ref={(input) => { this.messageBox = input; }} // on utilise la fonction ref pour assigner l'element à this.messageBox
           type="text"
           className="form-control"
           autoComplete="off"
